5794ba9fbfe2651e0a26638b80964c86846e80d2
Oksana Antonenko <wp1111.wp88@gmail.com>
2025-06-07 16:16:06 +0300

    work done

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,11 @@
-function getElementWidth(content, padding, border) {
-  const contentWith = Number.parseFloat(content);
-  const paddingSides = Number.parseFloat(padding);
-  const borderSides = Number.parseFloat(border);
-  return contentWith + 2 * paddingSides + 2 * borderSides;
+function checkForSpam(message) {
+  const lowercaseMessage = message.toLowerCase();
+  return lowercaseMessage.includes('spam') || lowercaseMessage.includes('sale');
 }
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(checkForSpam('Latest technology news')); // false
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+console.log(checkForSpam('Get best sale offers now!')); // true
+console.log(checkForSpam('Amazing SalE, only tonight!')); // true
+console.log(checkForSpam('Trust me, this is not a spam message')); // true
+console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
